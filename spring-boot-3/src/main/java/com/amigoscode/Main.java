9f86a3e9e91d5e75819576d40581ef61fd66d085
lalit <lalit@lalits-MacBook-Pro.local>
2022-12-03 13:39:49 +0530

    Understand @SpringBootApplication

--- spring-boot-3/src/main/java/com/amigoscode/Main.java
+++ spring-boot-3/src/main/java/com/amigoscode/Main.java
@@ -1,11 +1,18 @@
 package com.amigoscode;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+//@SpringBootApplication
+@ComponentScan(basePackages = "com.amigoscode")
+@EnableAutoConfiguration
+@Configuration
 @RestController
 public class Main {
     public static void main(String args[]){

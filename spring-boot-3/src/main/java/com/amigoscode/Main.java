50aa3cb9249510942d0537e53c650b43c508bd00
lalit <lalit@lalits-MacBook-Pro.local>
2022-12-03 21:19:07 +0530

    Getting Data In JSON format

--- spring-boot-3/src/main/java/com/amigoscode/Main.java
+++ spring-boot-3/src/main/java/com/amigoscode/Main.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Objects;
+
 //@SpringBootApplication
 @ComponentScan(basePackages = "com.amigoscode")
 @EnableAutoConfiguration
@@ -20,7 +23,45 @@ public class Main {
     }
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public GreetResponse greet(){
-        return new GreetResponse("Hello Geeks!");
+        return new GreetResponse(
+                "Hello Geeks!",
+                List.of("Java", "GoLang", "JavaScript"),
+                new Person("Lalit Siraswa")
+                );
     }
-    record GreetResponse(String greet){}
+    record Person(String name){}
+
+//    The Below both the GreetResponse Classes are exact same.
+    record GreetResponse(
+            String greet,
+            List<String> favProgrammingLanguages,
+            Person person
+    ){}
+
+//    class GreetResponse{
+//        private final String greet;
+//        public GreetResponse(String greet) {
+//            this.greet = greet;
+//        }
+//        public String getGreet() {
+//            return greet;
+//        }
+//        @Override
+//        public String toString() {
+//            return "GreetResponse{" +
+//                    "greet='" + greet + '\'' +
+//                    '}';
+//        }
+//        @Override
+//        public boolean equals(Object o) {
+//            if (this == o) return true;
+//            if (o == null || getClass() != o.getClass()) return false;
+//            GreetResponse that = (GreetResponse) o;
+//            return Objects.equals(greet, that.greet);
+//        }
+//        @Override
+//        public int hashCode() {
+//            return Objects.hash(greet);
+//        }
+//    }
 }

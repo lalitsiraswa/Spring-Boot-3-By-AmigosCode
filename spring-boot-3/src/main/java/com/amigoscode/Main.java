c36e3f982fbf00a0e7d3ef87498397197feb3a7d
lalit <lalit@lalits-MacBook-Pro.local>
2022-12-03 13:29:05 +0530

    Writing first API with springBoot

--- spring-boot-3/src/main/java/com/amigoscode/Main.java
+++ spring-boot-3/src/main/java/com/amigoscode/Main.java
@@ -1,10 +1,18 @@
 package com.amigoscode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String args[]){
         SpringApplication.run(Main.class, args);
+    }
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String greet(){
+        return "Hello Geeks!";
     }
 }

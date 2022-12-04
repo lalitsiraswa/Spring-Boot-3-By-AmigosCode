9691c8db811696d9a52c70cb931df17f8a9e83ab
lalit <lalit@lalits-MacBook-Pro.local>
2022-12-04 11:52:52 +0530

    Implemented GET API

--- spring-boot-3/src/main/java/com/amigoscode/Main.java
+++ spring-boot-3/src/main/java/com/amigoscode/Main.java
@@ -1,67 +1,31 @@
 package com.amigoscode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Objects;
 
-//@SpringBootApplication
-@ComponentScan(basePackages = "com.amigoscode")
-@EnableAutoConfiguration
-@Configuration
+//@ComponentScan(basePackages = "com.amigoscode")
+//@EnableAutoConfiguration
+//@Configuration
 @RestController
+@SpringBootApplication
+@RequestMapping("api/v1/customers")
 public class Main {
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String args[]){
         SpringApplication.run(Main.class, args);
     }
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public GreetResponse greet(){
-        return new GreetResponse(
-                "Hello Geeks!",
-                List.of("Java", "GoLang", "JavaScript"),
-                new Person("Lalit Siraswa", 23, 10000000)
-                );
+    @GetMapping
+    public List<Customer> getCustomer(){
+        return customerRepository.findAll();
     }
-    record Person(String name, int age, double savings){}
-
-//    The Below both the GreetResponse Classes are exact same.
-    record GreetResponse(
-            String greet,
-            List<String> favProgrammingLanguages,
-            Person person
-    ){}
-
-//    class GreetResponse{
-//        private final String greet;
-//        public GreetResponse(String greet) {
-//            this.greet = greet;
-//        }
-//        public String getGreet() {
-//            return greet;
-//        }
-//        @Override
-//        public String toString() {
-//            return "GreetResponse{" +
-//                    "greet='" + greet + '\'' +
-//                    '}';
-//        }
-//        @Override
-//        public boolean equals(Object o) {
-//            if (this == o) return true;
-//            if (o == null || getClass() != o.getClass()) return false;
-//            GreetResponse that = (GreetResponse) o;
-//            return Objects.equals(greet, that.greet);
-//        }
-//        @Override
-//        public int hashCode() {
-//            return Objects.hash(greet);
-//        }
-//    }
 }
